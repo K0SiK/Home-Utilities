b2d615d9bcd64013f90af8b37b1c43b587fff14d
K0SiK <Kosik@DESKTOP-9VIOPEO>
2017-01-15 12:08:46 +0100

    test merge

--- GitHub_test/GitHub_test/Form1.cs
+++ GitHub_test/GitHub_test/Form1.cs
@@ -16,5 +16,10 @@ namespace GitHub_test
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
